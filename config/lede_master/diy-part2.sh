0a273f4360a079e191347a61f5c1b1256d2620c9
Chen Xinyu <150147965+XingMaker@users.noreply.github.com>
2026-03-03 19:12:47 +0800

    Add UA2F and rkp-ipid packages

--- config/lede_master/diy-part2.sh
+++ config/lede_master/diy-part2.sh
@@ -51,6 +51,8 @@ fi
 # Add luci-app-amlogic
 rm -rf package/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+git clone https://github.com/EOYOHOO/UA2F.git package/UA2F
+git clone https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
 #
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
